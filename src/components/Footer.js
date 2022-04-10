57eea3404900b6d62f1b5fe4fac11682a31f6e28
Hady <hady.etd@gmail.com>
2022-04-10 12:37:39 -0400

    footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,14 +14,14 @@ function Footer(props) {
                    
                  </ul>
               </div>
-              <div className="col-8 col-sm-3 text-center mx-auto my-2">
+              <div className="col-8 col-sm-5 text-center mx-auto my-2">
                  <h5>Social</h5>
                  <a className="btn btn-social-icon btn-instagram" href="https://www.instagram.com/fcmauritania/?fbclid=IwAR2M1YcNiaTQypwPK5VU1N6-b2k7Yy0D_cElIn3v7oW8vaBhGPoQm1tBTlo" target="_blanc"><i className="fa fa-instagram" ></i></a>
                  <a className="btn btn-social-icon btn-facebook" href="https://www.facebook.com/FCMauritania" target="_blanc"><i className="fa fa-facebook"></i></a>
                  <a className="btn btn-social-icon btn-twitter" href="https://twitter.com/fcmauritania?fbclid=IwAR1T6HcSpavRyi1o_QIAh-HU38JimsbeUDgxCWR2MKBUpOQlsUhOYPbh1po" target="_blanc"><i className="fa fa-twitter" ></i></a>
                  <a className="btn btn-social-icon btn-google" href="https://www.youtube.com/channel/UCgFB4gNsc6Nk-cwsC7EdnoA" target="_blanc"><i className="fa fa-youtube"></i></a>
               </div>
-              <div className="col-8 col-sm-3 text-center mx-auto my-2">
+              <div className="col-8 col-sm-4 text-center mx-auto my-2">
                  <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone"></i> 32133868</a><br />
                  <a role="button" className="btn btn-link" href="mailto:freedomandcitizenshipngo@gmail.com" target="_blanc"><i className="fa fa-envelope-o"></i> Email us</a>
               </div>
